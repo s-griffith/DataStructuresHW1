d2615115daa73c9afe5727e585358fbc6e5072bf
Sarah <cmcgriffith@gmail.com>
2023-05-22 10:15:00 +0300

    removed print'

--- Group.cpp
+++ Group.cpp
@@ -97,7 +97,6 @@ Genre Group::find_max() const
     int max_views = 0;
     for (int i = 0; i < 4; i++) {
         if (m_totalViews[i] > max_views) {
-            std::cout << "m_totalViews " << i << "is " << m_totalViews[i] << std::endl;
             max_views = m_totalViews[i];
             max = static_cast<Genre>(i);
         }
